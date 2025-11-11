53ace949f4ade320100ebc1482a06dd17ee403cf
Chan Jian Le <jianle001@e.ntu.edu.sg>
2025-11-11 20:45:29 +0800

    wip: update nix flake

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "For fine tuning of a simple foundation model";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=release-24.05";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.micromamba
+          ];
+
+          shellHook = ''
+            # Set up micromamba in the shell
+            export MAMBA_ROOT_PREFIX="$PWD/.micromamba"
+            mkdir -p "$MAMBA_ROOT_PREFIX"
+
+            # Initialize micromamba for the current shell
+            # This only works with bash shell as shellHook is bash
+            CURR_SHELL=$(basename "$SHELL")
+            eval "$(micromamba shell hook --shell $CURR_SHELL)"
+
+            micromamba create -n light_weight_fine_tuning python pytorch torchvision torchaudio "cuda-version>=12.8,<13" datasets transformers peft evaluate accelerate jupyterlab -c pytorch -c conda-forge -c nvidia -y
+
+            echo "Micromamba development environment"
+            echo "Micromamba version: $(micromamba --version)"
+          '';
+        };
+      });
+    };
+}
